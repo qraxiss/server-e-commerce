a12dc36914124f292ab2eb6c7fa8ff3f8dada690
qraxiss <qraxiss@gmail.com>
2024-02-15 17:29:42 +0300

    Update createCategoryWithSlug.ts

--- src/resolvers/category/createCategoryWithSlug.ts
+++ src/resolvers/category/createCategoryWithSlug.ts
@@ -7,7 +7,6 @@ export const mutation = `
 export async function cretateCategoryWithSlug(obj, options, { context }) {
   let data = JSON.parse(JSON.stringify(options.data));
 
-  //   console.log(data);
   const result = await strapi
     .service("api::category.category")
     .createWithSlug(data);
