f788073a78c1257c353ffc260cd453484d14d889
qraxiss <qraxiss@gmail.com>
2024-02-24 22:45:56 +0300

    Update product.ts

--- src/api/product/services/product.ts
+++ src/api/product/services/product.ts
@@ -8,6 +8,21 @@ export default factories.createCoreService('api::product.product', ({ strapi }) 
             lower: true
         })
 
+        const product = await strapi.db.query('api::product.product').findOne({
+            where: {
+                slug: params.slug
+            }
+        })
+
+
+        if (product) {
+            params.slug = `${params.slug}-${randomstring.generate({
+                length: 3,
+                charset: 'alphabetic',
+                capitalization: 'lowercase'
+            })}`
+        }
+
         const result = await strapi.entityService.create('api::product.product', {
             data: params
         })
